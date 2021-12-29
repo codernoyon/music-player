311daaa3b31cd02f3febcf0c2a2a1a11cd9b9ee8
codernoyon <noyonrahman2003@gmail.com>
2021-12-29 23:28:20 +0600

    music player

---
+++ js/music-list.js
@@ -0,0 +1,125 @@
+let allMusic = [
+
+    {
+        name: "Bangla Folk Mashup",
+        artist: "Hasan S Iqbal",
+        img: "music-1",
+        src: "music-1"
+    },
+    {
+        name: "Valentine Mashup",
+        artist: "Ovi & Shreya",
+        img: "music-2",
+        src: "music-2"
+    },
+    {
+        name: "Bangla Mashup 2",
+        artist: "Hasna S. Iqbal & Parsha",
+        img: "music-3",
+        src: "music-3"
+    },
+    {
+        name: "Love Mashup 2021",
+        artist: "Hasan S. Iqbal & Dristy Anam",
+        img: "music-4",
+        src: "music-4"
+    },
+    {
+        name: "Premi 2.0",
+        artist: "Hasan S. Iqbal & Riddo Rongon",
+        img: "music-5",
+        src: "music-5"
+    },
+    {
+        name: "Ar Kibave",
+        artist: "Hasan S. Iqbal",
+        img: "music-6",
+        src: "music-6"
+    },
+    {
+        name: "Bangla Hit Mashup 2021",
+        artist: "Hasan S. Iqbal & Drist Anam",
+        img: "music-7",
+        src: "music-7"
+    },
+    {
+        name: "Valentine Love Mashup 2021",
+        artist: "Hasan S. Iqbal & Drist Anam",
+        img: "music-8",
+        src: "music-8"
+    },
+    {
+        name: "Porle mone X Zindi se",
+        artist: "Hasan S. Iqbal",
+        img: "music-9",
+        src: "music-9"
+    },
+    {
+        name: "Mujko Barsat banalo",
+        artist: "Hasan S. Iqbal & Drist Anam",
+        img: "music-10",
+        src: "music-10"
+    },
+    {
+        name: "CD Choice Hit Mashpu",
+        artist: "Hasan S. Iqbal & Drist Anam",
+        img: "music-11",
+        src: "music-11"
+    },
+    {
+        name: "Bangla Hasan Hit Mashup",
+        artist: "Hasan S. Iqbal",
+        img: "music-12",
+        src: "music-12"
+    },
+    {
+        name: "Abar",
+        artist: "Hasan S. Iqbal",
+        img: "music-13",
+        src: "music-13"
+    },
+    {
+        name: "Amar Gorur Garite",
+        artist: "Hasan S. Iqbal & Dristy Anam",
+        img: "music-14",
+        src: "music-14"
+    },
+    {
+        name: "Jodi Thakte Tomi",
+        artist: "Hasan S. Iqbal",
+        img: "music-15",
+        src: "music-15"
+    },
+    {
+        name: "O Mon re",
+        artist: "Tanveer Evan",
+        img: "music-16",
+        src: "music-16"
+    },
+    {
+        name: "O Piya re Piya",
+        artist: "Arjit Singh",
+        img: "music-17",
+        src: "music-17"
+    },
+    {
+        name: "Amar kache tomi onnorokom",
+        artist: "Imran Mahmodul",
+        img: "music-18",
+        src: "music-18"
+    },
+    {
+        name: "Rupkothar Jogote",
+        artist: "Unknown",
+        img: "music-19",
+        src: "music-19"
+    },
+    {
+        name: "Rataan Lambyan",
+        artist: "Jubin Natiyal",
+        img: "music-20",
+        src: "music-20"
+    },
+    
+    ];
+    
